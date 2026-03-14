010f1c65255f9748a3be07e2e8254a414138cc45
SHUBH-2007 <shubh20072001@gmail.com>
2026-03-14 13:17:03 +0530

    added hashing to passwords for safety

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
@@ -14,29 +15,62 @@ router.post(
       .withMessage("User ID must be between 5 and 12 characters"),
 
     body("email")
-    .notEmpty()
-    .withMessage("Email is required")
-    .isEmail()
-    .withMessage("Valid email required"),
-
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Valid email required"),
 
     body("password")
       .isLength({ min: 6 })
       .withMessage("Password must be at least 6 characters"),
   ],
 
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).json({
-        errors: errors.array(),
-      });
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    res.json({
-      message: "Validation passed",
-    });
+    const { user_id, email, password } = req.body;
+
+    try {
+      // Check duplicate user_id
+      db.get(
+        "SELECT * FROM users WHERE user_id = ? OR email = ?",
+        [user_id, email],
+        async (err, row) => {
+          if (row) {
+            return res.status(400).json({
+              message: "User ID or Email already exists",
+            });
+          }
+
+          // Hash password
+          const hashedPassword = await bcrypt.hash(password, 10);
+
+          // Insert user
+          db.run(
+            "INSERT INTO users (user_id, email, password) VALUES (?, ?, ?)",
+            [user_id, email, hashedPassword],
+            function (err) {
+              if (err) {
+                return res.status(500).json({ error: err.message });
+              }
+
+              res.json({
+                message: "User registered successfully",
+                user_id: user_id,
+              });
+            },
+          );
+        },
+      );
+    } catch (error) {
+      res.status(500).json({
+        message: "Server error",
+      });
+    }
   },
 );
 
