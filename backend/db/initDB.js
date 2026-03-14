93840abd660b5863e88c82e074c2b7bedb9fd4bf
SHUBH-2007 <shubh20072001@gmail.com>
2026-03-14 11:50:15 +0530

    database setup and user table added

---
+++ backend/db/initDB.js
@@ -0,0 +1,11 @@
+db.run(
+  `
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  )
+`,
+);
