e51687251ef46f9df0623c949178fe6053239511
SHUBH-2007 <shubh20072001@gmail.com>
2026-03-14 10:59:20 +0530

    basic server setup

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("CoreInventory Backend Running")
-})
+  res.send("CoreInventory Backend Running");
+});
 
-const PORT = 5000
+const PORT = 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
